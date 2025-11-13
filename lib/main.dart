0649e639898e4fd40fd426ea4e9852f522e9182a
Hachem Souyadi <hachemdsq@gmail.com>
2025-11-13 14:33:49 -0500

    nouvelle modification

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+      home: Scaffold(
+        body: Center(
+          
+          child: Column(
+            children: [
+              Text('Hello World!',style: TextStyle(fontSize: 24),),
+              Text('here its my first app',style: TextStyle(fontSize: 40),),
+              Container(
+                margin: EdgeInsets.all(20),
+                padding: EdgeInsets.all(16),
+                decoration: BoxDecoration(
+                  color: Colors.amberAccent,
+                  borderRadius: BorderRadius.circular(22),
+                ),
+                child: Text(
+                  'Boîte stylisée avec Container',
+                  style: TextStyle(color: Colors.white, fontSize: 18),
+                ))
+            ],
+            
+            
+          )
+          
+          
+        ),
+      ),
+    );
+  }
+}
